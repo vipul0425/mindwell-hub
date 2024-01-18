a2b64f38d3628ad9010b10292fe4843d5257f115
vipul Gupta <vipulgup04@gmail.com>
2024-01-18 19:42:58 +0530

    removes plugin suggestions.

--- themes/mindwell-theme/functions.php
+++ themes/mindwell-theme/functions.php
@@ -17,32 +17,3 @@ function mw_scripts() {
 }
 
 add_action( 'wp_enqueue_scripts', 'mw_scripts' );
-
-
-/**
- * Suggests a plugin to be activated after a theme is activated.
- */
-function suggest_plugin_after_theme_activation() {
-	// Check if the suggested plugin is not already active.
-	if ( ! is_plugin_active( 'mindWell-hub-vipul/mindwell.php' ) ) {
-		// Display an admin notice after theme activation.
-		add_action( 'admin_notices', 'suggest_plugin_notice_after_theme_activation' );
-	}
-}
-
-// Add the action to suggest the plugin.
-add_action( 'after_switch_theme', 'suggest_plugin_after_theme_activation' );
-
-
-/**
- * Displays a notice recommending the installation and activation of a plugin after the theme is activated.
- *
- * @return void
- */
-function suggest_plugin_notice_after_theme_activation() {
-	?>
-	<div class="notice notice-info is-dismissible">
-		<p><?php esc_html_e( 'For enhanced functionality, we recommend installing and activating the Plugin mindWell-hub-vipul. ', 'mindwell' ); ?><a href="https://drive.google.com/uc?id=1QLPiytUmPvz6-rZ34wvs3_NaQdxTL45e&export=download" download ><?php esc_html_e( 'Download Plugin', 'mindwell' ); ?></a></p>
-	</div>
-	<?php
-}
